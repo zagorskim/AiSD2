463cfcd3c61c14a93d8947de222e4ecd8eeade14
Mateusz Zagórski <01161597@pw.edu.pl>
2022-05-04 00:47:11 +0200

    Failed to optimize solution

--- Lab10/Lab10.cs
+++ Lab10/Lab10.cs
@@ -17,11 +17,11 @@ namespace Lab10
         /// <returns>Informację czy istnieje trasa oraz tablicę reprezentującą kolejne wierzchołki w trasie (pierwszy i ostatni element tablicy musi być 0). W przypadku, gdy zwracany jest false, wartość tego pola nie jest sprawdzana, może być null.</returns>
         public (bool routeExists, int[] route) PlanDelivery(Graph<int> railway, int[] eggDemand, int truckCapacity, int tankEngineRange, bool[] isRefuelStation, bool anySolution)
         {
-            //int[] a = { 5, 14, 24, 29};
+            //int[] a = { 5, 14, 24, 29 };
             //Console.WriteLine();
             //for (int i = 0; i < railway.VertexCount; i++)
             //{
-            //    if(a.Contains(i))
+            //    if (a.Contains(i))
             //        Console.ForegroundColor = System.ConsoleColor.Red;
             //    else
             //        Console.ForegroundColor = System.ConsoleColor.Blue;
@@ -46,22 +46,25 @@ namespace Lab10
             public int tankEngineRange;
             public bool[] isRefuelStation;
             public bool anySolution;
+            public int maxRange;
+            public int maxDist;
+            public int maxEggs;
 
             public List<int> currPath;
-            public bool[] currRefuels;
-            public int currEggsDelivered;
             public int currEggsOnboard;
             public int currRange;
             public Stack<int> currSold;
             public Stack<int> currDist;
+            public int currRangeLost;
+            public int currEggsLost;
 
             public List<int> resPath;
-            public bool[] resRefuels;
-            public int resEggsDelivered;
             public bool resExists;
 
             public int bestRange = 0;
             public int bestCount;
+            int distance = 0;
+            int count;
             public DeliveryPath(Graph<int> railway, int[] eggDemand, int truckCapacity, int tankEngineRange, bool[] isRefuelStation, bool anysolution)
             {
                 this.railway = railway;
@@ -73,18 +76,29 @@ namespace Lab10
             }
             public bool FindPath()
             {
+                int temp = tankEngineRange;
+                foreach (var i in isRefuelStation)
+                    if (i == true)
+                        temp += tankEngineRange;
+                maxRange = temp;
+                temp = 0;
+                foreach (var i in railway.DFS().SearchAll())
+                        temp += i.weight;
+                maxRange = temp;
+                temp = 0;
+                foreach (var i in railway.OutNeighbors(0))
+                    temp += truckCapacity;
+                maxEggs = temp;
+
                 currPath = new List<int>();
-                currRefuels = new bool[railway.VertexCount];
                 currRange = tankEngineRange;
-                currEggsDelivered = 0;
                 currEggsOnboard = truckCapacity;
                 currSold = new Stack<int>();
                 currDist = new Stack<int>();
+                currRangeLost = 0;
 
                 resPath = new List<int>();
-                resRefuels = new bool[railway.VertexCount];
                 resExists = false;
-                resEggsDelivered = 0;
 
                 DeliverEggs(0);
 
@@ -92,11 +106,16 @@ namespace Lab10
             }
             public void DeliverEggs(int place)
             {
-                if (anySolution && resExists) return;
+                if (anySolution && resExists)
+                    return;
+                int added1 = 0;
                 if (place == 0)
                 {
+                    added1 = truckCapacity - currEggsOnboard;
+                    currEggsLost += added1;
                     currEggsOnboard = truckCapacity;
-                    int count = 0, dist = 0, prev = 0;
+                    int dist = 0, prev = 0;
+                    count = 0;
                     foreach (var i in currPath)
                     {
                         if(count != 0) dist += railway.GetEdgeWeight(prev, i);
@@ -117,26 +136,34 @@ namespace Lab10
                 currSold.Push(currEggsOnboard);
 
                 currPath.Add(place);
-                int index = currPath.Count - 1;
 
+                int added2 = 0;
                 if(isRefuelStation[place])
                 {
+                    added2 = tankEngineRange - currRange;
+                    currRangeLost += tankEngineRange - currRange;
                     currRange = tankEngineRange;
                 }
                 currDist.Push(currRange);
 
                 ContinueTravel(place);
 
+                currEggsLost -= added1;
+                currRangeLost -= added2;
                 currSold.Pop();
-                currDist.Pop();
-                currPath.RemoveAt(index);
+                distance -= currDist.Pop();
+                currPath.RemoveAt(currPath.Count - 1);
             }
             public void ContinueTravel(int place)
             {
+                //if (currRangeLost > distance)
+                //    return;
+                if (maxEggs - currEggsLost < railway.VertexCount - (currPath.Count - count))
+                    return;
+                if (currPath.Count > bestCount && bestCount != 0 || currDist.Peek() == 0)
+                    return;
                 foreach(var e in railway.OutEdges(place))
                 {
-                    if (currPath.Count > bestCount && bestCount != 0 || currDist.Peek() == 0)
-                        break;
                     if (currPath.Contains(e.To) && e.To != 0)
                         continue;
                     if (currDist.Peek() >= e.weight && eggDemand[e.To] <= currSold.Peek())
@@ -144,6 +171,7 @@ namespace Lab10
                         currEggsOnboard = currSold.Peek();
                         currRange = currDist.Peek();
                         currRange -= e.weight;
+                        distance += e.weight;
                         DeliverEggs(e.To);
                     }
                 }
@@ -151,8 +179,6 @@ namespace Lab10
             public void CurrToRes()
             {
                 resPath = new List<int>(currPath);
-                for (int i = 0; i < isRefuelStation.Length; i++)
-                    resRefuels[i] = currRefuels[i];
                 resPath.Add(0);
             }
         }
