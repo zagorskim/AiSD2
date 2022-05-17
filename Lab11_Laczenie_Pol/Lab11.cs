d4b26dc8297c3b4eab9c1fb222a93d59b24407d7
Mateusz Zagórski <01161597@pw.edu.pl>
2022-05-17 22:14:24 +0200

    Prepared Lab11 tasks

---
+++ Lab11_Laczenie_Pol/Lab11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASD
+{
+    public class Lab11 : System.MarshalByRefObject
+    {
+
+        // iloczyn wektorowy
+        private int Cross((double, double) o, (double, double) a, (double, double) b)
+        {
+            double value = (a.Item1 - o.Item1) * (b.Item2 - o.Item2) - (a.Item2 - o.Item2) * (b.Item1 - o.Item1);
+            return Math.Abs(value) < 1e-10 ? 0 : value < 0 ? -1 : 1;
+        }
+
+        // Etap 1
+        // po prostu otoczka wypukła
+        public (double, double)[] ConvexHull((double, double)[] points)
+        {
+            var res = points.OrderBy<;
+            return null;
+        }
+
+        // Etap 2
+        // oblicza otoczkę dwóch wielokątów wypukłych
+        public (double, double)[] ConvexHullOfTwo((double, double)[] poly1, (double, double)[] poly2)
+        {
+            return null;
+        }
+
+    }
+}
