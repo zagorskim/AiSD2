e85f5b39304726727c84c8da5d4faec48442602c
Mateusz Zagórski <01161597@pw.edu.pl>
2022-05-19 22:44:26 +0200

    Almost finished Lab12 home part

---
+++ Lab12/Lab12.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ASD
+{
+    public class SheepHeard : MarshalByRefObject
+    {
+        /// <param name="dogs">Tablica opisującapozycje psow</param>
+        /// <param name="shed">Pozycja sciany budynku gospodarczego</param>
+        /// <returns>Wierzchołki wielokatku w którym owce są bezpieczne</returns>
+        public (double x, double y)[] SafePolygon((double x, double y)[] dogs, (float A, float B, float C) shed)
+        {
+            if (dogs.Length < 2)
+                return null;
+            var list = new List<(double, double)>(dogs);
+            list.Sort();
+            var help = new List<(double, double)>();
+            var s = new Solver(shed);
+            foreach(var i in list)
+            {
+                help.Add(s.OrtogonalProjection(i));
+            }
+            help.AddRange(dogs);
+            help.Sort();
+            var lower = new List<(double, double)>();
+            foreach(var i in help)
+            {
+                while (lower.Count >= 2 && s.Cross(lower[lower.Count - 2], lower[lower.Count - 1], i) <= 0)
+                    lower.RemoveAt(lower.Count - 1);
+                lower.Add(i);
+            }
+            help.Reverse();
+            var upper = new List<(double, double)>();
+            foreach(var i in help)
+            {
+                while (upper.Count >= 2 && s.Cross(upper[upper.Count - 2], upper[upper.Count - 1], i) <= 0)
+                    upper.RemoveAt(upper.Count - 1);
+                upper.Add(i);
+            }
+            upper.RemoveAt(upper.Count - 1);
+            lower.RemoveAt(lower.Count - 1);
+            lower.AddRange(upper);
+            return lower.ToArray();
+        }
+
+        /// <param name="sheeps">Tablica opisującapozycje owiec</param>
+        /// <param name="dogs">Tablica opisującapozycje psow</param>
+        /// <param name="shed">Pozycja sciany budynku gospodarczego</param>
+        /// <returns>Liczba bezpiecznych owiec</returns>
+        public int CheckCoverage((double x, double y)[] sheeps, (double x, double y)[] dogs, (float A, float B, float C) shed)
+        {
+            var s = new Solver(shed);
+            var sp = SafePolygon(dogs, shed);
+            if (sp == null)
+                return 0;
+            int curr = sp.Length / 2;
+            int prev1 = curr;
+            int prev2 = prev1;
+            int count = 0;
+            int flag;
+            int prevflag = 1;
+            foreach (var i in sheeps)
+            {
+                flag = 0;
+                prevflag = 1;
+                curr = sp.Length / 2;
+                prev1 = curr;
+                prev2 = prev1;
+                do
+                {
+                    if (s.Cross(sp[0], sp[curr], i) > 0)
+                    {
+                        prevflag = flag;
+                        flag = 1;
+                        //if (prevflag == -1)
+                        //{
+                        //    prev2 = prev1;
+                        //    prev1 = curr;
+                        //    curr++;
+                        //    break;
+                        //}
+                        //else
+                        //{
+                            prev2 = prev1;
+                            prev1 = curr;
+                            curr = (curr + sp.Length) / 2;
+                        //}
+                    }
+                    else if (s.Cross(sp[0], sp[curr], i) < 0)
+                    {
+                        prevflag = flag;
+                        flag = -1;
+                        //if (prevflag == -1)
+                        //{
+                        //    prev2 = prev1;
+                        //    prev1 = curr;
+                        //    curr++;
+                        //    break;
+                        //}
+                        //else
+                        //{
+                            prev2 = prev1;
+                            prev1 = curr;
+                            curr = curr / 2;
+                        //}
+                    }
+                    else
+                    {
+                        flag = 0;
+                        if (s.Distance(sp[curr], i) <= s.Distance(sp[0], sp[curr]) && s.Distance(sp[0], i) <= s.Distance(sp[0], sp[curr]))
+                            count++;
+                        break;
+                    }
+                }
+                while (curr != prev1 && flag + prevflag != 0);
+                if (flag == prevflag)
+                {
+                    if(flag == 1)
+                    {
+                        if (s.Cross(sp[prev1], sp[0], i) >= 0)
+                            count++;
+                    }
+                    else if (flag == -1)
+                    {
+                        if (s.Cross(sp[prev1], sp[sp.Length - 1], i) <= 0)
+                            count++;
+                    }
+                }
+                else if (flag == 1)
+                {
+                    if (s.Cross(sp[prev2], sp[prev1], i) <= 0)
+                        count++;
+                }
+                else if (flag == -1)
+                {
+                    if (s.Cross(sp[prev2], sp[prev1], i) >= 0)
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+    public class Solver
+    {
+        public (float A, float B, float C) eq;
+        public Solver((float A, float B, float C) e)
+        {
+            eq = e;
+        }
+        public (double, double) OrtogonalProjection((double, double) point)
+        {
+            if (eq.B == 0)
+                return (-eq.C / eq.A, point.Item2);
+            if (eq.A == 0)
+                return (point.Item1, -eq.C / eq.B);
+            return ((-1 *eq.A * eq.B * point.Item2 + Math.Pow(eq.B, 2) * point.Item1 - eq.A * eq.C) / (Math.Pow(eq.A, 2) + Math.Pow(eq.B, 2)),
+                ((-1 * eq.A * (-1 * eq.A * eq.B * point.Item2 + Math.Pow(eq.B, 2) * point.Item1 - eq.A * eq.C)) / (eq.B * (Math.Pow(eq.A, 2) + Math.Pow(eq.B, 2)))) - eq.C / eq.B);
+        }
+        public int Cross((double, double) o, (double, double) a, (double, double) b)
+        {
+            double value = (a.Item1 - o.Item1) * (b.Item2 - o.Item2) - (a.Item2 - o.Item2) * (b.Item1 - o.Item1);
+            return Math.Abs(value) < 1e-10 ? 0 : value < 0 ? -1 : 1;
+        }
+        public double Distance((double, double) point1, (double, double) point2)
+        {
+            return Math.Sqrt(Math.Pow(point1.Item1 - point2.Item1, 2) + Math.Pow(point1.Item2 - point2.Item2, 2));
+        }
+    }
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+    public class Stage1TestCase : TestCase
+    {
+        protected readonly (double x, double y)[] expectedResult;
+
+        protected readonly (double x, double y)[] dogs;
+        protected readonly (float A, float B, float C) shed;
+
+        protected (double x, double y)[] result;
+
+        public Stage1TestCase((double x, double y)[] dogs, (float A, float B, float C) shed, (double x, double y)[] expectedResult, double timeLimit, string description) : base(timeLimit, null, description)
+        {
+            this.dogs = dogs;
+            this.shed = shed;
+            this.expectedResult = expectedResult;
+        }
+
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            result = ((SheepHeard)prototypeObject).SafePolygon(dogs, shed);
+        }
+
+        protected override (Result resultCode, string message) VerifyTestCase(object settings)
+        {
+            var (code, msg) = checkSolution(result);
+            return (code, $"{msg} [{this.Description}]");
+        }
+
+        private (Result resultCode, string message) checkSolution((double x, double y)[] returnedResult)
+        {
+            if (returnedResult == null)
+            {
+                if (expectedResult == null)
+                    return OkResult("OK");
+                return (Result.WrongResult, $"Zwrócono null w przypadku kiedy bezpieczne miejsce istenieje");
+            }
+
+            if (returnedResult.Length != expectedResult.Length)
+            {
+                return (Result.WrongResult, $"Zwrócona tablica ma {returnedResult.Length} elementów, powinno być {expectedResult.Length}");
+            }
+
+            if (!expectedResult.ToHashSet().IsSubsetOf(returnedResult.Select(x => (Math.Round(x.x, 5), Math.Round(x.y, 5))).ToHashSet()))
+            {
+                return (Result.WrongResult, $"Zwrócona tablica zawiera błędne elementy");
+            }
+
+            return OkResult("OK");
+        }
+
+        public (Result resultCode, string message) OkResult(string message) => (TimeLimit < PerformanceTime ? Result.LowEfficiency : Result.Success, $"{message} {PerformanceTime.ToString("#0.00")}s");
+    }
+
+
+    public class Stage2TestCase : TestCase
+    {
+        protected readonly int expectedResult;
+
+        protected readonly (double x, double y)[] sheeps;
+        protected readonly (double x, double y)[] dogs;
+        protected readonly (float A, float B, float C) shed;
+
+        protected int result;
+
+        public Stage2TestCase((double x, double y)[] sheeps, (double x, double y)[] dogs, (float A, float B, float C) shed, int expectedResult, double timeLimit, string description) : base(timeLimit, null, description)
+        {
+            this.sheeps = sheeps;
+            this.dogs = dogs;
+            this.shed = shed;
+            this.expectedResult = expectedResult;
+        }
+
+        protected override void PerformTestCase(object prototypeObject)
+        {
+            result = ((SheepHeard)prototypeObject).CheckCoverage(sheeps, dogs, shed);
+        }
+
+        protected override (Result resultCode, string message) VerifyTestCase(object settings)
+        {
+            var (code, msg) = checkSolution(result);
+            return (code, $"{msg} [{this.Description}]");
+        }
+
+        private (Result resultCode, string message) checkSolution(int returnedResult)
+        {
+            if (expectedResult != returnedResult)
+            {
+                return (Result.WrongResult, $"Zwrócono {returnedResult}, powinno być {expectedResult}");
+            }
+
+            return OkResult("OK");
+        }
+
+        public (Result resultCode, string message) OkResult(string message) => (TimeLimit < PerformanceTime ? Result.LowEfficiency : Result.Success, $"{message} {PerformanceTime.ToString("#0.00")}s");
+    }
+
+
+    public class Lab12Tests : TestModule
+    {
+        TestSet Stage1 = new TestSet(prototypeObject: new SheepHeard(), description: "Etap 1, bezpieczny wielokat", settings: true);
+        TestSet Stage2 = new TestSet(prototypeObject: new SheepHeard(), description: "Etap 2, liczba bezpiecznych owiec", settings: true);
+
+        public override void PrepareTestSets()
+        {
+            TestSets["Stage1"] = Stage1;
+            TestSets["Stage2"] = Stage2;
+
+            prepare();
+        }
+
+
+
+
+        private void prepare()
+        {
+            (double x, double y)[] sheeps;
+            (double x, double y)[] dogs;
+            (float A, float B, float C) shed;
+            int expected_result;
+            (double x, double y)[] expected_polygon;
+
+            sheeps = new (double x, double y)[] { (0, 0) };
+            dogs = new (double x, double y)[] { };
+            shed = (1, 0, 0);
+            expected_result = 0;
+            expected_polygon = null;
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "No dogs"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "No dogs"));
+
+            sheeps = new (double x, double y)[] { (0, 0) };
+            dogs = new (double x, double y)[] { (0, 1) };
+            shed = (1, 0, 0);
+            expected_result = 0;
+            expected_polygon = null;
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "One dog"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "One dog"));
+
+            sheeps = new (double x, double y)[] { (-1, 0), (2, 0), (0.5, 0.5) };
+            dogs = new (double x, double y)[] { (1, 0), (1, 1) };
+            shed = (1, 0, 0);
+            expected_result = 1;
+            expected_polygon = new (double x, double y)[] { (0, 0), (0, 1), (1, 1), (1, 0) };
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Two dogs - square"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Two dogs - square"));
+
+            sheeps = new (double x, double y)[] { (-1, 0), (2, 0), (0.5, 0.5), (1, 1), (1.5, 0.5) };
+            dogs = new (double x, double y)[] { (1, 0), (2, 2) };
+            shed = (1, 0, 0);
+            expected_result = 2;
+            expected_polygon = new (double x, double y)[] { (0, 0), (0, 2), (2, 2), (1, 0) };
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Two dogs - trapezoid"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Two dogs - trapezoid"));
+
+
+            sheeps = new (double x, double y)[] { (1, 1), (1, 2), (0, 3), (3, 0), (3, 2), (3, 3), (4, 3), (1, 6) };
+            dogs = new (double x, double y)[] { (1, 0), (0, 1), (1, 3), (2, 2), (3, 4), (4, 1) };
+            shed = (1, 0, 1);
+            expected_result = 5;
+            expected_polygon = new (double x, double y)[] { (-1, 0), (-1, 4), (3, 4), (4, 1), (1, 0) };
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Simple example"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Simple example"));
+
+
+            sheeps = new (double x, double y)[] { (0, 0), (0, 4), (2, 1) };
+            dogs = new (double x, double y)[] { (1, 0), (0, 1), (1, 3), (2, 2), (3, 4), (4, 3) };
+            shed = (1, 0, 1);
+            expected_result = 3;
+            expected_polygon = new (double x, double y)[] { (-1, 0), (-1, 4), (3, 4), (4, 3), (1, 0) };
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Sheeps on egdes"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Sheeps on egdes"));
+
+            sheeps = new (double x, double y)[] { (0, 0), (1, 1), (1, 2), (0, 3), (3, 0), (3, 2), (3, 3), (4, 3) };
+            dogs = new (double x, double y)[] { (1, 0), (0, 1), (1, 3), (2, 2), (3, 4), (4, 1) };
+            shed = (0, 1, 1);
+            expected_result = 6;
+            expected_polygon = new (double x, double y)[] { (0, 1), (1, 3), (3, 4), (4, 1), (4, -1), (0, -1) };
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Horizontal line"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Horizontal line"));
+
+            sheeps = new (double x, double y)[] { (0, 0), (1, 1), (1, 2), (0, 3), (3, 0), (3, 2), (3, 3), (4, 3) };
+            dogs = new (double x, double y)[] { (1, 0), (0, 1), (1, 3), (2, 2), (3, 4), (4, 1) };
+            shed = (1, 1, 1);
+            expected_result = 6;
+            expected_polygon = new (double x, double y)[] { (-1.5, 0.5), (1, 3), (3, 4), (4, 1), (1, -2) };
+
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Diagonal line"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Diagonal line"));
+
+            sheeps = new (double x, double y)[] { (-0.8, 1.8), (-3.4, 3.5), (-5.2, 1.0), (-6.2, 3.9), (3.8, 9.8), (-5.8, 1.9), (-9.4, 2.4), (-8.6, 8.0), (-0.3, 1.6), (-8.3, -7.8), (6.2, -8.4), (-3.7, -4.7), (-0.2, -1.0), (-4.8, -3.1), (9.1, 7.8), (4.6, -6.6), (3.1, 0.7), (-7.9, 4.5), (9.6, -8.2), (4.8, -6.8), (4.0, 3.7), (-9.8, -6.5), (4.2, 0.6), (-4.9, 7.9), (7.1, 7.0), (-6.3, -7.4), (6.6, 7.3), (0.1, -7.2), (3.0, -6.0), (3.4, 5.6), (5.7, 4.4), (-1.1, 1.1), (7.5, -3.2), (-5.5, -9.9), (6.5, -5.4), (1.3, 1.2), (3.6, -5.7), (-6.7, 6.4), (0.4, 0.8), (6.3, 5.9), (-6.4, -0.5), (-9.0, -8.8), (-2.8, -3.5), (0.9, -4.5), (-2.3, 5.8), (-4.3, 7.4), (-2.9, -5.6), (9.4, -8.0), (-9.6, -6.1), (-1.9, -2.1), (8.1, 7.6), (6.8, 2.3), (-3.9, -9.7), (-5.3, -1.2), (8.4, 8.7), (-3.3, 2.6), (-1.4, 5.3), (-0.9, -9.5), (-1.8, 6.0), (-1.5, -7.3), (2.1, -0.6), (0.5, 4.7), (9.5, 8.6), (2.0, -4.2), (-4.4, -10.0), (7.2, 3.2), (-2.2, -2.0), (7.7, 8.2), (8.5, -2.7), (1.4, -9.1), (-7.1, 1.7), (-4.6, 4.9), (-1.7, 6.1), (5.1, -8.1), (-4.0, 8.8), (-8.7, -2.5), (0.2, -9.3), (-1.3, 0.3), (-2.6, 2.5), (-7.5, -9.2), (10.0, -2.4), (-8.5, 2.9), (-5.0, -0.7), (2.8, 2.7), (9.3, -3.6), (-8.9, -5.9), (-7.7, -7.6), (-7.0, -1.6), (6.7, -3.0), (3.3, 5.0), (-3.8, 1.5), (2.2, 5.2), (9.0, 4.3), (-4.1, 0.0), (-6.9, 5.4), (-5.1, 9.7), (4.1, 9.2), (9.9, 5.5), (-0.4, -0.1), (8.9, 8.3) };
+            dogs = new (double x, double y)[] { (-3.8, 7.1), (3.4, -4.7), (9.0, -0.1), (9.4, 7.7), (7.0, -9.0), (3.7, -3.2), (1.1, -1.9), (9.8, -1.1), (-9.6, -9.6), (8.9, -1.4), (8.9, 3.4), (2.9, -3.2), (-9.3, -6.5), (8.1, -2.6), (0.5, 1.9), (-1.9, 1.2), (-6.6, 1.7), (-4.5, 8.7), (-8.7, -10.0), (9.5, 1.2), (-4.3, 2.3), (5.1, -4.8), (-6.1, 0.2), (-8.5, -9.4), (-2.1, 2.5), (6.7, 8.0), (6.3, -2.9), (1.1, 6.9), (4.4, -3.0), (9.3, 9.7), (9.2, 6.7), (8.9, -8.0), (0.7, 4.5), (7.8, 1.6), (8.2, -1.7), (7.8, 8.3), (9.1, 6.8), (9.0, -2.6), (-2.3, 4.5), (-2.0, 8.3), (-1.1, 3.8), (-8.1, -3.4), (-6.6, -0.9), (-3.3, -0.2), (9.2, -9.3), (-0.1, -5.4), (6.4, -3.6), (4.8, 9.8), (1.6, 10.0), (-7.4, -5.0), (-1.7, 0.1), (-8.7, -0.2), (6.7, -1.0), (8.4, -0.4), (7.6, 5.0), (-4.9, 0.6), (6.0, 7.1), (-6.9, 7.1), (-3.6, -6.6), (-0.6, -1.0), (9.5, 5.9), (2.9, -6.0), (7.9, -1.1), (-5.7, 9.2), (3.0, 1.9), (1.9, -8.9), (-5.4, 7.3), (5.1, -8.3), (3.1, -6.0), (4.9, -6.8), (-5.6, -2.4), (1.8, 1.2), (-0.5, -9.1), (8.4, -9.1), (8.9, 0.6), (-4.8, 5.2), (1.5, 2.0), (5.3, -2.6), (9.1, 7.5), (-8.6, -9.7), (-3.6, -2.4), (1.0, -4.3), (6.9, -5.3), (7.4, 5.0), (-6.1, 0.2), (4.2, 5.1), (-6.0, 0.5), (-10.0, 7.1), (-1.0, -5.2), (6.7, -1.8), (4.0, 3.8), (-3.6, -7.1), (-10.0, 0.5), (9.5, 5.1), (1.0, 4.1), (3.3, 6.3), (0.6, -2.0), (7.8, -8.4), (8.4, 1.9), (8.9, -1.4) };
+            shed = (3, 3, 90);
+            expected_result = 93;
+            expected_polygon = new (double x, double y)[] { (9.2, -9.3), (9.8, -1.1), (9.5, 5.9), (9.3, 9.7), (1.6, 10), (-5.7, 9.2), (-10, 7.1), (-23.55, -6.45), (-5.75, -24.25) };
+            Stage1.TestCases.Add(new Stage1TestCase(dogs, shed, expected_polygon, timeLimit: 10, description: "Big example"));
+            Stage2.TestCases.Add(new Stage2TestCase(sheeps, dogs, shed, expected_result, timeLimit: 10, description: "Big example"));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new Lab12Tests();
+            tests.PrepareTestSets();
+            foreach (var ts in tests.TestSets)
+            {
+                ts.Value.PerformTests(verbose: true, checkTimeLimit: false);
+            }
+        }
+    }
+}
